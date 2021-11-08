a040c1fa5e114808374ca135d21d43e74fe77edf
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2021-11-08 10:06:32 +0100

    project added to repo

---
+++ myapp/src/test/java/dk/tec/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.tec.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
